b3b47c1d49019e397e6a11a4c75d3ce4689ad82e
llsahakyanll <sahakyansahak707@gmail.com>
2022-06-08 22:51:10 +0400

    x

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 window.onscroll = ()=>{
-    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    this.scrollY > 1 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
 const navbar = document.querySelector(".navbar");
+
+
+$('.section_1_nav ul li a').on('click', function(){
+    $('.section_1_nav ul li a').removeClass('section_1_nav_a_active');
+    $(this).addClass('section_1_nav_a_active');
+})
