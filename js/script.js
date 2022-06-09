ae39604010b711a471363d582ceed8e7fcff1824
llsahakyanll <sahakyansahak707@gmail.com>
2022-06-09 21:03:54 +0400

    Hamburger Menu

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ $('.section_1_nav ul li a').on('click', function(){
     $('.section_1_nav ul li a').removeClass('section_1_nav_a_active');
     $(this).addClass('section_1_nav_a_active');
 })
+
+function myFunction(x) {
+    // if (x.matches) { // If media query matches
+    //   $('header').html('')
+    // }else{
+    //     $('header').html('')
+    // }
+}
+  
+var x = window.matchMedia("(max-width: 760px)");
+myFunction(x);
+x.addListener(myFunction);
