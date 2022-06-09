0d77200e1e123f2518479db1192253ecbd352a14
llsahakyanll <sahakyansahak707@gmail.com>
2022-06-10 01:46:48 +0400

    i am tired

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 window.onscroll = ()=>{
-    this.scrollY > 1 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    if(this.scrollY > 1) {
+        $('.navbar').addClass('sticky')
+        $('.hamburger').addClass('sticky2')
+    }else{
+        $('.navbar').removeClass('sticky');
+        $('.hamburger').removeClass('sticky2')
+    }  
 }
-const navbar = document.querySelector(".navbar");
-
-
 $('.section_1_nav ul li a').on('click', function(){
     $('.section_1_nav ul li a').removeClass('section_1_nav_a_active');
     $(this).addClass('section_1_nav_a_active');
 })
-
+$('.nav_container').on('click', function(){
+    $('.hamburger_container').toggleClass('hamburger_container_active');
+    $('.line1').toggleClass('line1_active');
+    $('.line2').toggleClass('line2_active');
+    $('.line3').toggleClass('line3_active');
+    $('body').toggleClass('body_overflow');
+})
 function myFunction(x) {
-    // if (x.matches) { // If media query matches
-    //   $('header').html('')
-    // }else{
-    //     $('header').html('')
-    // }
+    if (x.matches) { // If media query matches
+        $('.navbar').addClass('none');
+        $('.hamburger').removeClass('none');
+        
+    }else{
+        $('.navbar').removeClass('none');
+        $('.hamburger').addClass('none');
+    }
 }
-  
 var x = window.matchMedia("(max-width: 760px)");
 myFunction(x);
 x.addListener(myFunction);
